31e089b823f47c7d2765751e4496e9fa8ab5a0f4
ximoske <ximoske@gmail.com>
2023-02-20 08:23:09 +0300

    preprod

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ export default class Tokenizer {
     }
 
     #free = () => {
-        if(this.#queue.length) this.#queue.shift()()
+        if(this.#queue.length) this.#queue.shift()!()
         else this.concurrency++
     }
 
